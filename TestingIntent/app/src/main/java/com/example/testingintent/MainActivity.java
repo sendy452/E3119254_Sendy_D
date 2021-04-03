d0bd543e3a63d17f8a6aa8830f5a0024c9e204f7
sendy452 <sendyivenyulian452@gmail.com>
2021-04-03 10:39:32 +0700

    Final

--- TestingIntent/app/src/main/java/com/example/testingintent/MainActivity.java
+++ TestingIntent/app/src/main/java/com/example/testingintent/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.testingintent;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int PICK_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE);
+            }
+        });
+
+        Button btn_reset = findViewById(R.id.reset);
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w) {
+                Toast toast = Toast.makeText(MainActivity.this, "Reset", Toast.LENGTH_LONG);
+                toast.show();
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            ImageView iGallery = findViewById(R.id.imageView);
+            // When an Image is picked
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) {
+                if (requestCode == PICK_IMAGE) {
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                        iGallery.setImageBitmap(selectedImage);
+
+                        //Pick image name from gallery
+                        Cursor returnCursor = getContentResolver().query(imageUri, null, null, null, null);
+                        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        returnCursor.moveToFirst();
+                        TextView nameView = findViewById(R.id.nama);
+                        nameView.setText(returnCursor.getString(nameIndex));
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
