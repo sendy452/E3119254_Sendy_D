a00fc96f8b61296a68595e3ecd4a90ce2fcbda07
sendy452 <sendyivenyulian452@gmail.com>
2021-04-21 09:19:53 +0700

    Done!

---
+++ ManajemenFIle/app/src/main/java/com/example/manajemenfile/Main2Activity.java
@@ -0,0 +1,76 @@
+package com.example.manajemenfile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class Main2Activity extends Activity {
+    TextView showText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        showText = findViewById(R.id.getText);
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void getPublic(View view) {
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File myFile = new File(folder, "myData1.txt");
+        String text = getdata(myFile);
+        if (text != null) {
+            showText.setText(text);
+        } else {
+            showText.setText("No Data");
+        }
+    }
+
+    private String getdata(File myfile) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(myfile);
+            int i = -1;
+            StringBuffer buffer = new StringBuffer();
+            while ((i = fileInputStream.read()) != -1) {
+                buffer.append((char) i);
+            }
+            return buffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public void getPrivate(View view) {
+        File folder = getExternalFilesDir("sendy");
+        File myFile = new File(folder, "myData2.txt");
+        String text = getdata(myFile);
+        if (text != null) {
+            showText.setText(text);
+        } else {
+            showText.setText("No Data");
+        }
+    }
+}
--- ManajemenFIle/app/src/main/java/com/example/manajemenfile/MainActivity.java
+++ ManajemenFIle/app/src/main/java/com/example/manajemenfile/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.manajemenfile;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int STORAGE_PERMISSION_CODE = 23;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText2);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+
+    public void savePublic(View view) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        String info = editText.getText().toString();
+        File myFile = new File(folder, "myData1.txt");
+        writeData(myFile, info);
+        editText.setText("");
+    }
+
+    public void savePrivate(View view) {
+        String info = editText.getText().toString();
+        File folder = getExternalFilesDir("sendy");
+        File myFile = new File(folder, "myData2.txt");
+        writeData(myFile, info);
+        editText.setText("");
+    }
+
+    private void writeData(File myFile, String data) {
+        FileOutputStream fileOutputStream = null;
+        try {
+            System.out.println("TES");
+            fileOutputStream = new FileOutputStream(myFile);
+            fileOutputStream.write(data.getBytes());
+            Toast.makeText(this, "Done" + myFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
